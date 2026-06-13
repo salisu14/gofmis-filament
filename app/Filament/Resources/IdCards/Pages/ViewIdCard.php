b781eca6be04e3c02ecec468a7309db54ab68dc6
Muhammad Salisu Ali <salisu14@yahoo.com>
2026-06-13 19:20:15 +0100

    Feat: Add preview and download actions in id card details page

--- app/Filament/Resources/IdCards/Pages/ViewIdCard.php
+++ app/Filament/Resources/IdCards/Pages/ViewIdCard.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Resources\IdCards\Pages;
 
 use App\Filament\Resources\IdCards\IdCardResource;
+use App\Models\IdCard;
+use Filament\Actions\Action;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -14,6 +16,22 @@ class ViewIdCard extends ViewRecord
     {
         return [
             EditAction::make(),
+
+            Action::make('preview')
+                ->label('Preview PDF')
+                ->icon('heroicon-o-eye')
+                ->color('info')
+                ->url(fn(IdCard $record): string => route('id-cards.preview', ['card' => $record]))
+                ->openUrlInNewTab(),
+
+            Action::make('download')
+                ->label('Download PDF')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->url(fn(IdCard $record) => route(
+                    'id-cards.download',
+                    ['idCard' => $record]
+                ))
+                ->openUrlInNewTab(),
         ];
     }
 }
